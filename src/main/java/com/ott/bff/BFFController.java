5c277145cf48bfc64f19ca99b7a3e7f59bb7383e
ondal <hiondal@gmail.com>
2022-01-06 21:50:30 +0900

    add logger, logging API url

--- src/main/java/com/ott/bff/BFFController.java
+++ src/main/java/com/ott/bff/BFFController.java
@@ -1,5 +1,7 @@
 package com.ott.bff;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("members")
 public class BFFController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Value("${member.host}")
     private String memberHost;
@@ -20,10 +23,14 @@ public class BFFController {
 
     @GetMapping("{id}")
     public Mono<String> getMember(@PathVariable String id) {
+
         WebClient client = WebClient.create();
 
+        String uri = "http://" + memberHost + ":" + memberPort + "/members/" + id;
+        log.info("### API URI: {}", uri);
+
         return client.get()
-                .uri("http://" + memberHost + ":" + memberPort + "/members/" + id)
+                .uri(uri)
                 .retrieve()
                 .bodyToMono(String.class);
     }
